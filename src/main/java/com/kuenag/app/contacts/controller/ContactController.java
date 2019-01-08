c1b49a061016e13a548a08aa594083998b741b70
Alvaro Andres Cruz <Andres.CruzBurbano@wnco.com>
2019-01-08 16:33:25 -0500

    implementing dependency inyection with Autowired anotation

--- src/main/java/com/kuenag/app/contacts/controller/ContactController.java
+++ src/main/java/com/kuenag/app/contacts/controller/ContactController.java
@@ -4,6 +4,7 @@ import com.kuenag.app.contacts.entity.Contact;
 import com.kuenag.app.contacts.service.ReadData;
 import com.kuenag.app.contacts.utils.Constants;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,11 +24,8 @@ import java.util.List;
 @Slf4j
 public class ContactController {
 
-    private ReadData readContactList;
-
-    public ContactController(ReadData readContactList) {
-        this.readContactList = readContactList;
-    }
+    @Autowired
+    ReadData readContactList;
 
     /**
      * This method provides a contact list according to the parameter value of
